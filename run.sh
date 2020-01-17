fc2312b9190647f877f8605a76020fddefd426e7
Steve Farrell <sparticlesteve@gmail.com>
2020-01-16 23:20:00 -0800

    initial commit of files

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -C gpu
+#SBATCH -N 1
+#SBATCH --gres=gpu:8
+#SBATCH --exclusive
+#SBATCH -t 10
+
+module load gcc/7.3.0
+module load cuda/10.1.168
+module load nccl/2.4.8
+module load openmpi/4.0.1-ucx-1.6
+module load pytorch/v1.3.1-gpu
+
+export UCX_LOG_LEVEL=debug
+export UCX_NET_DEVICES=mlx5_0:1,mlx5_2:1,mlx5_4:1,mlx5_6:1
+export OMPI_MCA_btl_smcuda_use_cuda_ipc=0
+export OMPI_MCA_btl=^openib,tcp
+export OMPI_MCA_osc=ucx
+
+srun -n 2 -c 10 -u -l python test_mpi_cuda.py
---
+++ test_mpi_cuda.py
@@ -0,0 +1,33 @@
+import torch
+import torch.distributed as dist
+
+# Configuration
+ranks_per_node = 8
+shape = 2**17
+dtype = torch.float32
+
+# Initialize MPI
+dist.init_process_group(backend='mpi')
+rank, n_ranks = dist.get_rank(), dist.get_world_size()
+local_rank = rank % ranks_per_node
+
+# Allocate a small tensor on every gpu from every rank.
+# This is an attempt to force creation of all device contexts.
+#for i in range(ranks_per_node):
+#    _ = torch.randn(1).to(torch.device('cuda', i))
+
+# Select our gpu
+device = torch.device('cuda', local_rank)
+print('MPI rank', rank, 'size', n_ranks, 'device', device)
+
+# Allocate a tensor on the gpu
+x = torch.randn(shape, dtype=dtype).to(device)
+print('local result:', x.sum())
+
+# Do a broadcast from rank 0
+dist.broadcast(x, 0)
+print('broadcast result:', x.sum())
+
+# Do an all-reduce
+dist.all_reduce(x)
+print('allreduce result:', x.sum())
